9201e783bfa280f4ba17636235c958ac25d180f3
Bruno Deprez <u0130626@kuleuven.be>
2023-01-18 11:14:13 +0100

    Bug fix

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from HelperFunctions import load_network, feature_engineering
 import excecute
 
-def run_model():
+def run_model(fraud_node_tf):
     # Start by initialising the data
     HG, labels, claim_data = load_network(fraud_node_tf=False)
-    HG_F, labels, claim_data = load_network(fraud_node_tf=True)
+    HG_F, labels, claim_data = load_network(fraud_node_tf=fraud_node_tf)
     
     # Calculate all resutls for BiRank and generate figures
     pred_bi, fpr_bi, tpr_bi, res_bi = excecute.BiRank_subroutine(HG, labels)
 
     # Calculate all resutls for BiRank and generate figures
-    pred_meta, fpr_meta, tpr_meta, res_meta = excecute.Metapath2Vec_subroutine(HG_F, labels)
+    pred_meta, fpr_meta, tpr_meta, res_meta = excecute.Metapath2Vec_subroutine(HG_F, labels, fraud_node_tf=fraud_node_tf)
     
     # Feature engineering on claim specific data + selection of features
     claim_data_features = feature_engineering(claim_data)
@@ -21,7 +21,7 @@ def run_model():
 
 if __name__ == '__main__':
     print("Runnig the model...")
-    run_model() #run all models
+    run_model(fraud_node_tf = True) #run all models
     print("All done!")
 
 
